0724e1c1c72564f0a7c5595c198582d237ec9250
Ethan Knox <ethan.knox@hyphen.ai>
2024-01-19 13:20:50 -0500

    need to dig here more later

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*", "docs", "docs.*"]),
     zip_safe=False,
     install_requires=[
-        'pydantic~=2.3.3',
+        'pydantic>=2.0.1',
         'pydantic_settings~=2.0.0',
-        'httpx~=0.25.0',
+        'httpx>=0.25.0',
         'json_log_formatter~=0.5.2',
     ],
     python_requires=">=3.6",
