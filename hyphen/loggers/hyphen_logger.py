fb23b564178c43de9db6dacd7aa522b209dc0e9b
Ethan Knox <ethan.knox@hyphen.ai>
2024-02-12 16:39:16 -0500

    logging better best practices

--- hyphen/loggers/hyphen_logger.py
+++ hyphen/loggers/hyphen_logger.py
@@ -1,45 +1,10 @@
 from typing import Optional
-import logging.config
-import sys
+import logging
 
-LOGGING = {
-    "version": 1,
-    "formatters": {
-        "verbose": {
-            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-        },
-        "curt": {
-            "class": "hyphen.loggers.curt_formatter.CurtFormatter",
-            "format": "%(levelname)s %(asctime)s.%(msecs)03d %(filename)s:%(lineno)d %(message)s",
-            "datefmt": "%-I:%M:%S",
-        },
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "stream": sys.stdout,
-            "formatter": "curt",
-            "filters": ["healthcheck"],
-        }
-    },
-    "filters": {
-        "healthcheck": {
-            "()": "hyphen.loggers.health_check_filter.HealthcheckFilter",
-        }
-    },
-    "loggers": {
-        "hyphen": {
-            "level": "INFO",
-            "handlers": ["console"],
-            "propagate": False,
-        }
-    },
-}
 def get_logger(name: str, level:Optional[str]=None):
-    if level:
-        LOGGING["loggers"]["hyphen"]["level"] = level
-    logging.config.dictConfig(LOGGING)
+    """See library best practices for logging: https://docs.python.org/3/howto/logging.html#library-config"""
     logger = logging.getLogger("hyphen")
+    logger.addHandler(logging.NullHandler())
+    if level:
+        logger.setLevel(level)
     return logger.getChild(name)
-    return logger
