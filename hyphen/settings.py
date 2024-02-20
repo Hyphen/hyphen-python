b1a4ddd906c506cd8327a666139f911a4cad3954
Ethan Knox <ethan.knox@hyphen.ai>
2024-02-20 12:09:24 -0500

    remove unrequired settings elements

--- hyphen/settings.py
+++ hyphen/settings.py
@@ -6,10 +6,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     hyphen_client_id: Optional[str] = None
     hyphen_client_secret: Optional[str] = None
-    development_hyphen_uri: AnyHttpUrl = "https://engine.dev.hyphen.ai"
-    local_hyphen_uri: AnyHttpUrl = "http://localhost:8000"
-    local_hyphen_db_username: str = None
-    local_hyphen_db_password: str = None
 
 # singleton
 settings = Settings()
