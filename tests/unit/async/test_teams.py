6ae28ca0922323ec006d6d58069a765630e28cae
Ethan Knox <ethan.knox@hyphen.ai>
2024-02-28 20:00:46 -0500

    teams add/remove

--- tests/unit/async/test_teams.py
+++ tests/unit/async/test_teams.py
@@ -1,8 +1,6 @@
 import pytest_asyncio
 from pytest import mark as m
-from faker import Faker
 
-fake = Faker()
 from hyphen import HyphenClient
 
 
@@ -17,7 +15,7 @@ class TestTeamsAsync:
 
     @m.vcr()
     async def test_add_team(self, client):
-        name = fake.company()
+        name = "Red Team"
         team = await client.teams.create(name=name)
         assert team.name == name
         assert team.id is not None
@@ -25,4 +23,10 @@ class TestTeamsAsync:
         teams = await client.teams.list()
         assert team.id in [x.id for x in teams]
 
-
+    @m.vcr()
+    async def test_remove_team(self, client):
+        teams = await client.teams.list()
+        marketing = [x for x in teams if x.name == "marketing"][0]
+        await client.teams.delete(marketing)
+        teams = await client.teams.list()
+        assert marketing.id not in [x.id for x in teams]
