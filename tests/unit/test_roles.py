cec347472da0d8458b9622e0e9d43a5e7ec77297
Ethan Knox <ethan.knox@hyphen.ai>
2024-02-29 13:00:04 -0500

    roles equal their string

---
+++ tests/unit/test_roles.py
@@ -0,0 +1,11 @@
+from pytest import mark as m
+from hyphen.roles import Role
+
+@m.describe("Roles")
+class TestRole:
+
+    @m.it("should be equal to a string with the same value")
+    def test_role_equals_string(self):
+        role = Role(name="teamOwner", context="team", context_id="123")
+        assert role == "teamOwner"
+        assert not role == "teamMember"
